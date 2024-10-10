257cfab265bd4136ec6f70024438e538936ffe02
Juhasz013 <dzseksz.szperro@gmail.com>
2024-10-10 21:32:17 +0200

    asdasd

--- src/views/Kartya.vue
+++ src/views/Kartya.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 karakter-kartyak">
+    <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 ">
       <KarakterKartya v-for="karakter in szurtKarakterek" :key="karakter.id" :id="karakter.id"
         @karaterModalReszletKezeles="reszletModalKezelo" class="karakter-kartya">
         <template v-slot:kep>
